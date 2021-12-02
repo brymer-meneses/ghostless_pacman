34403e9bb60ae34e61a8dac4d15395a41a728a08
Brymer <meneses.brymer@gmail.com>
2021-12-02 23:01:41 +0800

    feat: add `SDL2_mixer` to CMake dependencies

--- cmake/handle_dependencies.cmake
+++ cmake/handle_dependencies.cmake
@@ -41,13 +41,20 @@ if (WIN32)
         URL             https://www.libsdl.org/projects/SDL_image/release/SDL2_image-devel-2.0.5-mingw.tar.gz
         SOURCE_DIR      ${PROJECT_SOURCE_DIR}/external/SDL2_image
     )
+
+    FetchContent_Declare(
+        SDL2_mixer
+        URL             https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-devel-2.0.4-mingw.tar.gz
+        SOURCE_DIR      ${PROJECT_SOURCE_DIR}/external/SDL2_mixer
+        )
     
-    FetchContent_MakeAvailable(SDL2 SDL2_image)
+    FetchContent_MakeAvailable(SDL2 SDL2_image SDL2_mixer)
     set(ARCH x86_64-w64-mingw32)
 
     include_directories(
         ${PROJECT_NAME}
         ${PROJECT_SOURCE_DIR}/external/SDL2_image/${ARCH}/include/
+        ${PROJECT_SOURCE_DIR}/external/SDL2_mixer/${ARCH}/include/
         ${PROJECT_SOURCE_DIR}/external/SDL2/${ARCH}/include/
         ${PROJECT_SOURCE_DIR}/external/SDL2/${ARCH}/include/SDL2
         )
@@ -56,28 +63,41 @@ if (WIN32)
         ${PROJECT_SOURCE_DIR}/external/SDL2/${ARCH}/lib/libSDL2.dll.a
         ${PROJECT_SOURCE_DIR}/external/SDL2/${ARCH}/lib/libSDL2main.a
         ${PROJECT_SOURCE_DIR}/external/SDL2_image/${ARCH}/lib/libSDL2_image.dll.a
+        ${PROJECT_SOURCE_DIR}/external/SDL2_mixer/${ARCH}/lib/libSDL2_mixer.dll.a
     )
 
-    file(COPY 
-        ${PROJECT_SOURCE_DIR}/external/SDL2_image/${ARCH}/bin/SDL2_image.dll
-        DESTINATION
-        ${PROJECT_SOURCE_DIR}/bin/
-    )
+    file(GLOB 
+        SDL2_IMAGE_DLLS
+        ${PROJECT_SOURCE_DIR}/external/SDL2_image/${ARCH}/bin/*.dll
+        )
+
+    file(GLOB
+        SDL2_MIXER_DLLS
+        ${PROJECT_SOURCE_DIR}/external/SDL2_mixer/${ARCH}/bin/*.dll
+        )
+
+
+    # Copy required DLLs to the bin directory. 
+    # This ensures that every DLL file required can be accessed by 
+    # the game
+
+    # SDL
     file(COPY 
         ${PROJECT_SOURCE_DIR}/external/SDL2/${ARCH}/bin/SDL2.dll
         DESTINATION
         ${PROJECT_SOURCE_DIR}/bin/
     )
+    # SDL Image
     file(COPY 
-        ${PROJECT_SOURCE_DIR}/external/SDL2_image/${ARCH}/bin/libpng16-16.dll
-        DESTINATION
+        ${SDL2_IMAGE_DLLS}
         ${PROJECT_SOURCE_DIR}/bin/
-    )
+        )
+    # SDL Mixer
     file(COPY 
-        ${PROJECT_SOURCE_DIR}/external/SDL2_image/${ARCH}/bin/zlib1.dll
-        DESTINATION
+        ${SDL2_MIXER_DLLS}
         ${PROJECT_SOURCE_DIR}/bin/
-    )
+        )
+
 endif ()
 
 
